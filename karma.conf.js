b98d741db4c3d790dd1ec203798bdc38ea897e73
ysgk <ysgk@users.noreply.github.com>
2019-09-19 17:40:31 +0900

    GitHub actions 追加

---
+++ karma.conf.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    exclude: [],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    concurrency: Infinity,
+    files: ['./karma-entry.ts'],
+    preprocessors: {
+      '**/*.ts': ['webpack'],
+      '**/*.tsx': ['webpack'],
+    },
+    webpack: {
+      mode: 'development',
+      resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx'],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'babel-loader',
+          },
+        ],
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.test': true,
+        }),
+      ],
+    },
+    customLaunchers: {
+      ie_no_addons: {
+        base: 'IE',
+        flags: ['-extoff'],
+      },
+    },
+  })
+}
