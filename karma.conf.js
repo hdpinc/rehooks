d48d5bb0ed9957602ab2df41ed9f894408dced28
ysgk <ysgk@users.noreply.github.com>
2019-10-15 17:47:10 +0900

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,32 @@
 const webpack = require('webpack')
-
-// Karma configuration
-// Generated on Thu Sep 19 2019 17:43:04 GMT+0900 (GMT+09:00)
-
 module.exports = function(config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
-    files: ['./polyfills.ts', './enzyme.ts', './packages/**/*.spec.ts', './packages/**/*.spec.tsx'],
-
-    // list of files / patterns to exclude
     exclude: [],
-
-    //plugins
-    plugins: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    concurrency: Infinity,
+    files: ['./polyfills.ts', './enzyme.ts', 'packages/**/*.spec.ts', 'packages/**/*.spec.tsx'],
     preprocessors: {
       '**/*.ts': ['webpack'],
       '**/*.tsx': ['webpack'],
     },
-
     webpack: {
       mode: 'development',
       resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
+        extensions: ['.js', '.json', '.ts', '.tsx'],
       },
       module: {
         rules: [
           {
             test: /\.tsx?$/,
-            loader: 'babel-loader',
+            use: 'babel-loader',
           },
         ],
       },
@@ -47,44 +36,11 @@ module.exports = function(config) {
         }),
       ],
     },
-
-    // define browsers
     customLaunchers: {
       ie_no_addons: {
         base: 'IE',
         flags: ['-extoff'],
       },
     },
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // 対象ブラウザはコマンドラインから与える
-    browsers: ['ChromeHeadless'],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
   })
 }
