0ef054c228d3cdeaecec642d8168f906f27596e1
ysgk <ysgk@users.noreply.github.com>
2019-10-15 18:04:31 +0900

    enzyme shallow

--- packages/use-bool/index.spec.tsx
+++ packages/use-bool/index.spec.tsx
@@ -1,5 +1,5 @@
+import { shallow } from 'enzyme'
 import React from 'react'
-import { shallow } from '../../enzyme'
 import useBool from './index'
 
 const TestComponent: React.FC<{ initialValue: boolean }> = (props) => {
