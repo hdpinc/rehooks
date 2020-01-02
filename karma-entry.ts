bc390f7c4efcdb6143edbf9a61a0d92d97c4aa45
ysgk <ysgk@users.noreply.github.com>
2020-01-03 01:25:54 +0900

    polyfill の修正

--- karma-entry.ts
+++ karma-entry.ts
@@ -1,21 +1,21 @@
-// 順番が重要。polyfill の読み込みが先!
+// Polyfill for 3rd-party codes
 // ===============================
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 // ===============================
 
-const testsContext = require.context('./src/', true, /.*spec\.tsx?$/)
-
-testsContext.keys().forEach(testsContext)
-
-// These are settings to run jest tests on karma.
+// These are settings to run jest on karma.
 // https://www.kabuku.co.jp/developers/run-jest-on-browsers
 // https://github.com/kimamula/jest-karma-angular-demo/blob/master/src/test.ts
 window['jest'] = require('jest-mock')
 window['expect'] = require('expect')
 
-// This is polyfill for IE
+// This is a polyfill to run @testing-library/react on IE
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#polyfill
 if (!Element.prototype.matches) {
   Element.prototype.matches = (Element.prototype as any).msMatchesSelector
 }
+
+const testsContext = require.context('./src/', true, /.*spec\.tsx?$/)
+
+testsContext.keys().forEach(testsContext)
