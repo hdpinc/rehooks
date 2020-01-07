fad8c8796ca8324c2ebd4dfee99b0c05cc9b4777
ysgk <ysgk@users.noreply.github.com>
2020-01-07 14:38:51 +0900

    feat!: fix interface

--- src/use-datepicker/utils.ts
+++ src/use-datepicker/utils.ts
@@ -3,7 +3,11 @@ import { addDays, getDay, getDaysInMonth, startOfWeek } from 'date-fns'
 /**
  * カレンダー作る用
  */
-export const getDateMatrixForYearMonth = (year: number, month: number): Date[][] => {
+export const getDateMatrixForYearMonth = <T = Date>(
+  year: number,
+  month: number,
+  mapper: (date: Date) => T = (date) => (date as any) as T
+): T[][] => {
   const startDate = startOfWeek(new Date(year, month, 1))
   const startOfMonth = new Date(year, month, 1)
   const rows = Math.ceil((getDaysInMonth(startOfMonth) + getDay(startOfMonth)) / 7)
@@ -12,9 +16,9 @@ export const getDateMatrixForYearMonth = (year: number, month: number): Date[][]
   return (
     Array.from({ length })
       // create a list of dates
-      .map((_, index) => addDays(startDate, index))
+      .map((_, index) => mapper(addDays(startDate, index)))
       // fold the array into a matrix
-      .reduce<Date[][]>(
+      .reduce<any[][]>(
         (matrix, current, index, days) =>
           !(index % cols !== 0) ? [...matrix, days.slice(index, index + cols)] : matrix,
         []
