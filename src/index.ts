a7583e4bcb547a851b78ab2ed837862b6973edf4
ysgk <ysgk@users.noreply.github.com>
2020-01-30 19:03:24 +0900

    fix(use-toggle): :bug: export use-toggle

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import useKeyDown from './use-key-down'
 import useKeyUp from './use-key-up'
 import useDate from './use-date'
 import useDatepicker from './use-datepicker'
+import useToggle from './use-toggle'
 
-export { useBool, useClickAway, useDate, useDatepicker, useKeyDown, useKeyUp }
+export { useBool, useClickAway, useDate, useDatepicker, useKeyDown, useKeyUp, useToggle }
