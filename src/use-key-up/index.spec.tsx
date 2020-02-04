4a4bc9a5a5b3db7f5979b158264d4b8dc6379a56
ysgk <ysgk@users.noreply.github.com>
2020-02-04 21:38:49 +0900

    chore: wip

--- src/use-key-up/index.spec.tsx
+++ src/use-key-up/index.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, createEvent } from '@testing-library/react'
 import useKeyUp from './index'
 
 const TestComponent: React.FC<{ pressKey: string }> = (props) => {
@@ -17,12 +17,14 @@ const TestComponent: React.FC<{ pressKey: string }> = (props) => {
 
 it('handler should be called when Esc key keyup.', () => {
   const { getByTestId } = render(<TestComponent pressKey={'Esc'} />)
-  fireEvent.keyDown(window, { key: 'Esc' })
+  const event = createEvent.keyDown(window, { key: 'Esc' })
+  fireEvent(window, event)
   expect(getByTestId('state').textContent).toBe('on')
 })
 
 it('handler should not be called when another key is keyup.', () => {
   const { getByTestId } = render(<TestComponent pressKey={'Esc'} />)
-  fireEvent.keyDown(window, { key: 'Ctrl' })
+  const event = createEvent.keyDown(window, { key: 'Ctrl' })
+  fireEvent(window, event)
   expect(getByTestId('state').textContent).toBe('off')
 })
