d2a2bf16a33b89b5b71e2ed87ff8ee4ce2aad1ac
ysgk <ysgk@users.noreply.github.com>
2020-05-11 21:18:22 +0900

    test(use-local-storage): :green_heart: fix tests

--- src/use-local-storage/index.spec.tsx
+++ src/use-local-storage/index.spec.tsx
@@ -1,8 +1,5 @@
 import { act, renderHook } from '@testing-library/react-hooks'
-import localStorage from './localStorage'
 import useLocalStorage from './index'
-
-jest.mock('./localStorage')
 
 it('should return initial value', () => {
   const { result } = renderHook(() => useLocalStorage('key', ''))
@@ -17,13 +14,14 @@ it('should return stored value if one exists instead of initial value', () => {
   expect(state).toBe('stored')
 })
 
-it('should update value', () => {
-  jest.spyOn(localStorage, 'setItem')
-  const { result } = renderHook(() => useLocalStorage('key', ''))
-  const [_, setState] = result.current
-  act(() => {
-    setState('updated')
+describe('set', () => {
+  it('should update value', () => {
+    const { result } = renderHook(() => useLocalStorage('key', ''))
+    const [_, setState] = result.current
+    act(() => {
+      setState('updated')
+    })
+    expect(result.current[0]).toBe('updated')
+    expect(localStorage.getItem('key')).toBe(JSON.stringify('updated'))
   })
-  expect(result.current[0]).toBe('updated')
-  expect(localStorage.getItem('key')).toBe(JSON.stringify('updated'))
 })
