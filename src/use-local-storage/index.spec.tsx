771de6ca17166ff0ef9d943e8dcbe73a2cdf7ba7
ysgk <ysgk@users.noreply.github.com>
2020-05-12 18:56:38 +0900

    feat(use-local-storage): :sparkles: add useLocalStorage (#266)

---
+++ src/use-local-storage/index.spec.tsx
@@ -0,0 +1,32 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import localStorage from './localStorage'
+import useLocalStorage from './index'
+
+afterEach(() => {
+  localStorage.clear()
+})
+
+it('should return initial value', () => {
+  const { result } = renderHook(() => useLocalStorage('key', ''))
+  const [state] = result.current
+  expect(state).toBe('')
+})
+
+it('should return stored value if one exists instead of initial value', () => {
+  localStorage.setItem('key', JSON.stringify('stored'))
+  const { result } = renderHook(() => useLocalStorage('key', ''))
+  const [state] = result.current
+  expect(state).toBe('stored')
+})
+
+describe('set', () => {
+  it('should update value', () => {
+    const { result } = renderHook(() => useLocalStorage('key', ''))
+    const [_, setState] = result.current
+    act(() => {
+      setState('updated')
+    })
+    expect(result.current[0]).toBe('updated')
+    expect(localStorage.getItem('key')).toBe(JSON.stringify('updated'))
+  })
+})
