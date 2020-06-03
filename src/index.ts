6d5a843b30de1e6fdcdc97c7d26e3f106ffde1b2
ysgk <ysgk@users.noreply.github.com>
2020-06-03 18:40:35 +0900

    fix: add useLocalStorage to index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import useDate from './use-date'
 import useDatepicker from './use-datepicker'
 import useKeyDown from './use-key-down'
 import useKeyUp from './use-key-up'
+import useLocalStorage from './use-local-storage'
 import useToggle from './use-toggle'
 
-export { useArray, useBool, useClickAway, useDate, useDatepicker, useKeyDown, useKeyUp, useToggle }
+export { useArray, useBool, useClickAway, useDate, useDatepicker, useKeyDown, useKeyUp, useLocalStorage, useToggle }
